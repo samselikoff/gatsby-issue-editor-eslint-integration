8002a750c52cc165d99f7a937285afc1e4b7abac
Sam Selikoff <sam.selikoff@gmail.com>
2019-07-22 08:16:16 -0400

    add unused variable

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+let x
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
